ddf0cfd8f153f833bcf2c63e5297943108313ee9
Espresso521 <kotaku20220424@gmail.com>
2022-09-20 23:15:29 +0900

    Add a button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+
+  const [num, setNum] = useState(1)
+
+  const fn = () => {
+    setNum(num+1)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello 胡牧之！！</h1>
+        <h1>Hello My Son! I Love U!</h1>
+        <h2>Number is : {num}</h2>
+        <button onClick={fn}>Add</button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
